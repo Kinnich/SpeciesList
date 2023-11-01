aa5acdc92d30219bb0d81645c1aa3989843bca4a
Kinnich <ardnik@gmail.com>
2023-11-01 10:38:23 -0500

    update prompt, readme, and autoupdate place search

--- ai_util.py
+++ ai_util.py
@@ -9,19 +9,24 @@ openai.api_base = "https://api.pulze.ai/v1" # enter Pulze's URL
 
 # url = "http://en.wikipedia.org/wiki/Raccoon"
 
-prompt = """Read this wiki page and give 3 short bullet points:
-    - an interesting fact about animal
-    - where on the landscape is it most often found?
-    - what it eats
+prompt = """What signs can I use to track this animal?
+    - What do the tracks look like?
+    - Describe the scat
+    - What feeding signs would this animal leave?
+    - Where does the animal make it home/ bed/ nest/ den?
+    - In which seasons is the animal active?
+    Return response organized as these bullets: 
+    "**Tracks**", "**Scat**", "**Feeding_signs**", 
+    "**Home**", "**Seasonal Activity**"
     """ 
 @st.cache_data
-def llm_summarize(article: str) -> str:
+def llm_summarize(animal: str) -> str:
     response = openai.ChatCompletion.create(
         model="pulze-v0",
-        max_tokens=100,
+        max_tokens=300,
         messages=[{
         "role": "user",
-        "content": (prompt + article),
+        "content": (prompt + animal),
         }],
     )
     return response['choices'][0]['message']['content']
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from st_keyup import st_keyup
 import pandas as pd
 from ai_util import llm_summarize
 from iNat_util import get_iNat_locations, get_iNat_species_count
@@ -23,30 +24,29 @@ with st.sidebar:
     st.markdown('## Choose a location and animal category')
     # TODO: add a note about how autocomplete works - and City of Austin is better than Austin for whatever reason
     # and Memphis Metropolitan Area works
-    location = st.text_input('Enter location of interest - city, county, or well known landmark', '')
-    
-    if location:
+    location = st_keyup('Enter location of interest - city, county, or well known landmark', '')
+    # if location:
         # Get list of valid locations and id's from iNaturalist given user input location
-        possible_locations = get_iNat_locations(location)
-        best_match_loc = st.selectbox(
-            'Choose the best match:',
-            options=possible_locations.keys(),
-            key="best_loc_match",
-            index=None,
+    possible_locations = get_iNat_locations(location)
+    best_match_loc = st.selectbox(
+        'Choose the best match:',
+        options=possible_locations.keys(),
+        key="best_loc_match",
+        # index=None,
+    )
+
+    if best_match_loc:
+        # Get the iNaturalist id for the location (needed to query the species lists)
+        place_id = possible_locations[best_match_loc]
+        st.session_state.place_id = place_id
+
+        # Set an animal group
+        animal_class = st.selectbox(
+            'What class of animals are you interested in?',
+            ['Mammalia', 'Amphibia', 'Reptilia', 'Aves', 'Insecta', 'Mollusca'],
+            key="animal_class",
+            # index=None,
         )
-
-        if best_match_loc:
-            # Get the iNaturalist id for the location (needed to query the species lists)
-            place_id = possible_locations[best_match_loc]
-            st.session_state.place_id = place_id
-
-            # Set an animal group
-            animal_class = st.selectbox(
-                'What class of animals are you interested in?',
-                ['Mammalia', 'Amphibia', 'Reptilia', 'Aves', 'Insecta', 'Mollusca'],
-                key="animal_class",
-                index=None,
-            )
 # Main page displays table only once location and animal group are selected from sidebar
 if st.session_state.best_loc_match and st.session_state.animal_class:
 
@@ -64,33 +64,39 @@ if st.session_state.best_loc_match and st.session_state.animal_class:
         df = pd.DataFrame.from_dict(normalized)
 
         df_short = df[
-            ['taxon.default_photo.medium_url',
-             'taxon.preferred_common_name',
-             'taxon.name',
-             'count',
-             'taxon.wikipedia_url']
+            [
+                'taxon.default_photo.medium_url',
+                'taxon.preferred_common_name',
+                'taxon.name',
+                'count',
+            ]
         ].copy()
-
-        # df_short.index = df_short.index + 1
 
         number_species = len(df)
         st.text(f'There are {number_species} species observed in this location')
 
-        # TODO: cache openai calls 
-        # df_short['Summary'] = df_short['taxon.wikipedia_url'].apply(llm_summarize)
-        
         st.dataframe(
             df_short,
             column_config={
                 'taxon.default_photo.medium_url': st.column_config.ImageColumn(label='Image', width='small'),
                 'taxon.preferred_common_name': 'Common Name',
                 'taxon.name': 'Latin Name',
-                'count': st.column_config.NumberColumn(label='Observations', width='small'),
-                'taxon.wikipedia_url': st.column_config.LinkColumn(label='Wikipedia', width='small'),
-                # 'Summary': st.column_config.TextColumn(label='Quick Facts (double click to view)', width='large'),
+                'count': st.column_config.NumberColumn(label='Observations', width='medium'),
             },
             hide_index=True,
         )
+
+        # Get tracking info for a selected animal
+        animal = st.selectbox('Select an animal to learn more about', options=df_short['taxon.preferred_common_name'].tolist())
+        photo_link = df_short[df_short['taxon.preferred_common_name'] == animal]['taxon.default_photo.medium_url'].iloc[0]
+        latin_name = df_short[df_short['taxon.preferred_common_name'] == animal]['taxon.name'].iloc[0]
+        col1, col2 = st.columns(2)
+        with col1:
+            st.image(photo_link)
+            st.markdown(f"_{latin_name}_")
+        
+        with col2:
+            col2.write(llm_summarize(animal))
 else:
     st.write("Choose a location on the sidebar")
 
